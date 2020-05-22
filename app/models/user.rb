c161f976f936253381abb1352c24d0b9909b3d7f
yukito.nakajima <yukito.nakajima@ntt.com>
2020-05-22 03:03:10 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,9 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end
+  
+  # タイムラインの実装
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
 end
