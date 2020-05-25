fae75691d724a7ad72898fed3c236bddc4a28843
yukito.nakajima <yukito.nakajima@ntt.com>
2020-05-25 07:38:27 +0000

    navbar likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   
   
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
+    @users = User.order(id: :desc).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page]).per(5)
     counts(@user)
   end
 
